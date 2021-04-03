7f7c8d17452dda0ccda59039a02ceda42039b76f
Bapt5 <drouillet.baptiste@gmail.com>
2021-04-03 19:09:17 +0200

    api Pronote

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+import pronotepy  # api Pronote
+from pronotepy.ent import ile_de_france
 
 # Création de l'application
 app = Flask(__name__)
+
+'''connexion a l'api Pronote avec l'username et le mdp ENT mais je suis pas sur que ca va etre possible'''
+# client = pronotepy.Client('https://0910626l.index-education.net/pronote/eleve.html',
+#                           username=username,
+#                           password=mdpENT,
+#                           ent=ile_de_france)
 
 
 # Quand on arrive sur le site, on affiche la page "ma_page.html"
