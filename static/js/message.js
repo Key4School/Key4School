801946c041afca8506b8ac6c37e429c5da3bebfd
Flavien <flavcodeur@gmail.com>
2021-04-11 19:03:51 +0200

    update

--- static/js/message.js
+++ static/js/message.js
@@ -3,16 +3,15 @@ $(document).ready(function() {
       height: (80 / 100 * ($(window).height())).toString() + 'px',
       width: (75 / 100 * ($(window).width())).toString() + 'px',
     });
-
+  function envoi(){
+    var donnees = $('#messageForm').serialize();
+    $.ajax({
+      url: '/messages/', // on donne l'URL du fichier de traitement
+      type: "POST", // la requête est de type POST
+      data: donnees, // et on envoie nos données
+      success: function(response) {
+        $('#messageForm').trigger("reset");
+      },
+    });
+  }
 });
-function envoi(){
-  var donnees = $('#messageForm').serialize();
-  $.ajax({
-    url: '/messages/', // on donne l'URL du fichier de traitement
-    type: "POST", // la requête est de type POST
-    data: donnees, // et on envoie nos données
-    success: function(response) {
-      $('#messageForm').trigger("reset");
-    },
-  });
-}
