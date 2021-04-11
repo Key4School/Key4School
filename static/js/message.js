ec7c5cde3bfd0a755a0e83770a96790626ac4d9f
Bapt5 <drouillet.baptiste@gmail.com>
2021-04-11 19:33:36 +0200

    pas envoyer des messages vides

--- static/js/message.js
+++ static/js/message.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
       width: (75 / 100 * ($(window).width())).toString() + 'px',
     });
   function envoi(){
-    var donnees = $('#messageForm').serialize();
-    $.ajax({
-      url: '/messages/', // on donne l'URL du fichier de traitement
-      type: "POST", // la requête est de type POST
-      data: donnees, // et on envoie nos données
-      success: function(response) {
-        $('#messageForm').trigger("reset");
-      },
-    });
+    if($('#envoimsg').val()!=''){
+      var donnees = $('#messageForm').serialize();
+      $.ajax({
+        url: '/messages/', // on donne l'URL du fichier de traitement
+        type: "POST", // la requête est de type POST
+        data: donnees, // et on envoie nos données
+        success: function(response) {
+          $('#messageForm').trigger("reset");
+        },
+      });
+    }
   }
 });
