1dea71a86af0828d6dddf3055d14a19261eced8b
Flavien <flavcodeur@gmail.com>
2021-04-12 10:57:43 +0200

    bandeau sur message

--- static/js/message.js
+++ static/js/message.js
@@ -2,22 +2,10 @@ $(document).ready(function() {
   $('#titreetmsg').css({
       height: (80 / 100 * ($(window).height())).toString() + 'px',
       width: (85 / 100 * ($(window).width())).toString() + 'px',
-    });
-    $('#nomgroupe').css({
-        height: (90 / 100 * ($(window).height())).toString() + 'px',
-      });
-      function divmenu() {
-        if ($('#multichoix').css("display") == 'none') {
-          $('#multichoix').css({
-            display: "block",
-          });
-          $('#page').append('<div id="filtre" onclick="divmenu()" style="opacity:0;"><div>');
-        } else {
-          $('#multichoix').css({
-            display: "none",
-          });
-        }
-      }
+  });
+  $('#nomgroupe').css({
+      height: (90 / 100 * ($(window).height())).toString() + 'px',
+  });
   function envoi(){
     if($('#envoimsg').val()!=''){
       var donnees = $('#messageForm').serialize();
