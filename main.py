20c3e3a025cb50da611091cc7b2936f2321fb503
Bapt5 <drouillet.baptiste@gmail.com>
2021-04-12 20:23:58 +0200

    pas envoi de message si pas GET id

--- main.py
+++ main.py
@@ -62,10 +62,12 @@ def messages():
         if 'id' in request.args:
             msgDb = db_messages.find({'id-groupe': request.args["id"]})
             idgroupe = request.args["id"]
-            infogroupes = db_groupes.find_one({"_id": ObjectId(request.args["id"]) })
+            infogroupes = db_groupes.find_one(
+                {"_id": ObjectId(request.args["id"])})
         else:
-            msgDb = []
-            idgroupe = ''
+            msgDb = None
+            idgroupe = None
+            infogroupes = None
         return render_template("messages.html", msgDb=msgDb, grpUtilisateur=grp, idgroupe=idgroupe, infogroupe=infogroupes)
 
     elif request.method == 'POST':
