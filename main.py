54477b0d4372ff88308a51670910fd21888ca870
Bapt5 <drouillet.baptiste@gmail.com>
2021-04-13 19:06:39 +0200

    fixe msg groupe

--- main.py
+++ main.py
@@ -64,15 +64,14 @@ def messages(idGroupe):
         if request.method == 'GET':
             # il faudra récupérer l'id qui sera qans un cookie
             grp = db_groupes.find(
-                {'id-utilisateurs': ObjectId("60731a7115be24651a803e20")})  # on remplacera le numéro par l'id de l'user
+                {'id-utilisateurs': ObjectId("6075cae8fb56bf0654e5f4ab")})  # on remplacera le numéro par l'id de l'user
             if idGroupe != None:
-                msgDb = db_messages.find({'id-groupe': idGroupe})
+                msgDb = db_messages.find({'id-groupe': ObjectId(idGroupe)})
                 idgroupe = idGroupe
                 infogroupes = db_groupes.find_one(
                     {"_id": ObjectId(idGroupe)})
                 infoUtilisateurs = []
                 for content in infogroupes['id-utilisateurs']:
-                    print(content)
                     infoUtilisateurs += db_utilisateurs.find(
                         {"_id": ObjectId(content)})
             else:
@@ -83,7 +82,7 @@ def messages(idGroupe):
             return render_template("messages.html", msgDb=msgDb, grpUtilisateur=grp, idgroupe=idgroupe, infogroupe=infogroupes, infoUtilisateurs=infoUtilisateurs)
 
         elif request.method == 'POST':
-            db_messages.insert_one({"id-groupe": request.form['group'], "id-utilisateur": "quand on l'aura",
+            db_messages.insert_one({"id-groupe": ObjectId(request.form['group']), "id-utilisateur": ObjectId(session['id']),
                                     "contenu": request.form['contenuMessage'], "date-envoi": datetime.now(), "img": ""})
             return 'sent'
     else:
@@ -134,18 +133,19 @@ def professeur():
 def question():
     if 'id' in session:
         if request.method == 'POST':
-            if 'demande' not in request.form :
-                result = db_demande_aide.find({'$text' : {'$search':request.form['research']}})
-                    
-                return render_template('question.html', answer = result)
-            else :
-                db_demande_aide.insert_one({"id-utilisateur": "quand on l'aura", "titre": request.form['titre'], "contenu": request.form['demande'], "date-envoi": datetime.now(), "matière": request.form['matiere']})
-                return render_template('question.html', envoi = "Envoi réussi")
-        else :
+            if 'demande' not in request.form:
+                result = db_demande_aide.find(
+                    {'$text': {'$search': request.form['research']}})
+
+                return render_template('question.html', answer=result)
+            else:
+                db_demande_aide.insert_one(
+                    {"id-utilisateur": "quand on l'aura", "titre": request.form['titre'], "contenu": request.form['demande'], "date-envoi": datetime.now(), "matière": request.form['matiere']})
+                return render_template('question.html', envoi="Envoi réussi")
+        else:
             return render_template('question.html')
     else:
         return redirect(url_for('login'))
-
 
 
 @app.route('/amis/')
