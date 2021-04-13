92a7ac1f0fbe833a1c3dc26ce9f9ee844341f734
Bapt5 <drouillet.baptiste@gmail.com>
2021-04-13 14:10:07 +0200

    connexion plus obligation de se connecter pour acceder aux pages

--- main.py
+++ main.py
@@ -43,89 +43,121 @@ db_groupes = cluster.db.groupes
 # Quand on arrive sur le site, on affiche la page "ma_page.html"
 @app.route('/')
 def accueil():
-    return render_template("index.html")
+    if 'id' in session:
+        return render_template("index.html")
+    else:
+        return redirect(url_for('login'))
 
 
 # laisser le nom entre deux slash ca permet d'accepter toutes les urls du style http://127.0.0.1:3000/messages/ sinon ca marche pas.s
 @app.route('/accueil/')
 def accueil2():
-    return render_template("index.html")
+    if 'id' in session:
+        return render_template("index.html")
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/messages/', defaults={'idGroupe': None}, methods=['POST', 'GET'])
 @app.route('/messages/<idGroupe>', methods=['POST', 'GET'])
 def messages(idGroupe):
-    if request.method == 'GET':
-        # il faudra récupérer l'id qui sera qans un cookie
-        grp = db_groupes.find(
-            {'id-utilisateurs': ObjectId("60731a7115be24651a803e20")})  # on remplacera le numéro par l'id de l'user
-        if idGroupe != None:
-            msgDb = db_messages.find({'id-groupe': idGroupe})
-            idgroupe = idGroupe
-            infogroupes = db_groupes.find_one(
-                {"_id": ObjectId(idGroupe)})
-            infoUtilisateurs = []
-            for content in infogroupes['id-utilisateurs']:
-                print(content)
-                infoUtilisateurs += db_utilisateurs.find(
-                    {"_id": ObjectId(content)})
-        else:
-            msgDb = None
-            idgroupe = None
-            infogroupes = None
-            infoUtilisateurs = None
-        return render_template("messages.html", msgDb=msgDb, grpUtilisateur=grp, idgroupe=idgroupe, infogroupe=infogroupes, infoUtilisateurs=infoUtilisateurs)
+    if 'id' in session:
+        if request.method == 'GET':
+            # il faudra récupérer l'id qui sera qans un cookie
+            grp = db_groupes.find(
+                {'id-utilisateurs': ObjectId("60731a7115be24651a803e20")})  # on remplacera le numéro par l'id de l'user
+            if idGroupe != None:
+                msgDb = db_messages.find({'id-groupe': idGroupe})
+                idgroupe = idGroupe
+                infogroupes = db_groupes.find_one(
+                    {"_id": ObjectId(idGroupe)})
+                infoUtilisateurs = []
+                for content in infogroupes['id-utilisateurs']:
+                    print(content)
+                    infoUtilisateurs += db_utilisateurs.find(
+                        {"_id": ObjectId(content)})
+            else:
+                msgDb = None
+                idgroupe = None
+                infogroupes = None
+                infoUtilisateurs = None
+            return render_template("messages.html", msgDb=msgDb, grpUtilisateur=grp, idgroupe=idgroupe, infogroupe=infogroupes, infoUtilisateurs=infoUtilisateurs)
 
-    elif request.method == 'POST':
-        db_messages.insert_one({"id-groupe": request.form['group'], "id-utilisateur": "quand on l'aura",
-                                "contenu": request.form['contenuMessage'], "date-envoi": datetime.now(), "img": ""})
-        return 'sent'
+        elif request.method == 'POST':
+            db_messages.insert_one({"id-groupe": request.form['group'], "id-utilisateur": "quand on l'aura",
+                                    "contenu": request.form['contenuMessage'], "date-envoi": datetime.now(), "img": ""})
+            return 'sent'
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/profil/')
 def profil():
-    return render_template("profil.html")
+    if 'id' in session:
+        return render_template("profil.html")
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/archives/')
 def archives():
-    return render_template("archives.html")
+    if 'id' in session:
+        return render_template("archives.html")
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/classe/')
 def classe():
-    return render_template("classe.html")
+    if 'id' in session:
+        return render_template("classe.html")
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/monlycee/')
 def monlycee():
-    return render_template("monlycee.html")
+    if 'id' in session:
+        return render_template("monlycee.html")
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/professeur/')
 def professeur():
-    return render_template("professeur.html")
+    if 'id' in session:
+        return render_template("professeur.html")
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/question/')
 def question():
-    return render_template("question.html")
+    if 'id' in session:
+        return render_template("question.html")
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/amis/')
 def amis():
-    return render_template("amis.html")
+    if 'id' in session:
+        return render_template("amis.html")
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/deconnexion/')
 def deconnexion():
-    return 'Ca va redirigé vers la connexion'
+    for s in session:
+        session[s] = None
+    return redirect(url_for('login'))
 
 
 # TOUT LE CODE QUI VA SUIVRE PERMET LA CONNEXION A L'ENT VIA OAUTH
 # Route qui va permettre de rediriger l'utilisateur sur le site d'authentification et de récupérer un token (pour pouvoir se connecter)
 @app.route("/login/")
-def demo():
+def login():
     """Step 1: User Authorization.
     Redirect the user/resource owner to the OAuth provider (ENT)
     using an URL with a few key OAuth parameters.
@@ -175,14 +207,14 @@ def connexion():
         'https://ent.iledefrance.fr/auth/oauth2/userinfo').json()
     user = db_utilisateurs.find_one({"idENT": data['userId']})
     if user != None:
-        session['id'] = user['_id']
+        session['id'] = str(user['_id'])
         session['pseudo'] = user['pseudo']
     else:
-        id = ObjectId()
-        db_utilisateurs.insert_one({"_id": id, "idENT": data['userId'], "nom": data['lastName'], "prenom": data['firstName'], "pseudo": data['username'],
+        db_utilisateurs.insert_one({"idENT": data['userId'], "nom": data['lastName'], "prenom": data['firstName'], "pseudo": data['username'],
                                     "dateInscription": datetime.now(), "birth_date": data['birthDate'], "classe": data['level'], "lycee": data['schoolName']})
-        session['id'] = id
-        session['pseudo'] = data['username']
+        user = db_utilisateurs.find_one({"idENT": data['userId']})
+        session['id'] = str(user['_id'])
+        session['pseudo'] = user['pseudo']
     return redirect(url_for('accueil'))
 
 
