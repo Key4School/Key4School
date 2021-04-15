0738ce8df654d1f75aa842974fce118259e0ee26
Jaadee_ <jade1mass@hotmail.com>
2021-04-15 21:11:24 +0200

    ajout cases profil

--- static/js/profil.js
+++ static/js/profil.js
@@ -5,10 +5,39 @@ function inputpseudo(){
   }
 
 }
-
 function inputemail(){
   if($('#email')!==undefined){
     var email= $('#email').html(); // renvoie le texte contenu à l'intérieur du paragraphe
     $('#contentEmail').html('<input class="input" id="inputEmail" type="email" value="' + email + '"/>'); // remplace le code HTML actuel par celui-ci
+  }
+}
+function inputinstagram(){
+  if($('#instagram')!==undefined){
+    var instagram= $('#instagram').html(); // renvoie le texte contenu à l'intérieur du paragraphe
+    $('#contentInstagram').html('<input class="input" id="inputInstagram" type="text" value="' + instagram + '"/>'); // remplace le code HTML actuel par celui-ci
+  }
+}
+function inputsnapchat(){
+  if($('#snapchat')!==undefined){
+    var snapchat= $('#snapchat').html(); // renvoie le texte contenu à l'intérieur du paragraphe
+    $('#contentSnapchat').html('<input class="input" id="inputSnapchat" type="text" value="' + snapchat + '"/>'); // remplace le code HTML actuel par celui-ci
+  }
+}
+function inputphone(){
+  if($('#phone')!==undefined){
+    var phone= $('#phone').html(); // renvoie le texte contenu à l'intérieur du paragraphe
+    $('#contentPhone').html('<input class="input" id="inputPhone" type="text" value="' + phone + '"/>'); // remplace le code HTML actuel par celui-ci
+  }
+}
+function inputinteret(){
+  if($('#interet')!==undefined){
+    var interet= $('#interet').html(); // renvoie le texte contenu à l'intérieur du paragraphe
+    $('#contentInteret').html('<input class="input" id="Interet" type="text" value="' + interet + '"/>'); // remplace le code HTML actuel par celui-ci
+  }
+}
+function inputlv(){
+  if($('#lv')!==undefined){
+    var lv= $('#lv').html(); // renvoie le texte contenu à l'intérieur du paragraphe
+    $('#contentLv').html('<input class="input" id="Lv" type="text" value="' + lv + '"/>'); // remplace le code HTML actuel par celui-ci
   }
 }
