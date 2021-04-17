d728913997fb13c5038ee52b4df5ab3d40f5e8d2
Flavien <flavcodeur@gmail.com>
2021-04-17 16:25:27 +0200

    input dans profil

--- static/js/profil.js
+++ static/js/profil.js
@@ -1,55 +1,6 @@
-function inputpseudo(){
-  if($('#pseudo')!==undefined){
-    var pseudo= $('#pseudo').html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#contentPseudo').html('<input class="input" name="pseudo" id="inputPseudo" type="text" value="'+ pseudo + '"/>'); // remplace le code HTML actuel par celui-ci
-  }
-
-}
-function inputemail(){
-  if($('#email')!==undefined){
-    var email= $('#email').html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#contentEmail').html('<input class="input" name="email" id="inputEmail" type="email" value="' + email + '"/>'); // remplace le code HTML actuel par celui-ci
-  }
-}
-function inputinstagram(){
-  if($('#instagram')!==undefined){
-    var instagram= $('#instagram').html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#contentInstagram').html('<input class="input" name="insta" id="inputInstagram" type="text" value="' + instagram + '"/>'); // remplace le code HTML actuel par celui-ci
-  }
-}
-function inputsnapchat(){
-  if($('#snapchat')!==undefined){
-    var snapchat= $('#snapchat').html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#contentSnapchat').html('<input class="input" name="snap" id="inputSnapchat" type="text" value="' + snapchat + '"/>'); // remplace le code HTML actuel par celui-ci
-  }
-}
-function inputphone(){
-  if($('#phone')!==undefined){
-    var phone= $('#phone').html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#contentPhone').html('<input class="input" name="telephone" id="inputPhone" type="text" value="' + phone + '"/>'); // remplace le code HTML actuel par celui-ci
-  }
-}
-function inputinteret(){
-  if($('#interet')!==undefined){
-    var interet= $('#interet').html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#contentInteret').html('<input class="input" name="interet" id="Interet" type="text" value="' + interet + '"/>'); // remplace le code HTML actuel par celui-ci
-  }
-}
-function inputlv(){
-  if($('#lv')!==undefined){
-    var lv= $('#lv').html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#contentLv').html('<input class="input" name="langues" id="Lv" type="text" value="' + lv + '"/>'); // remplace le code HTML actuel par celui-ci
-  }
-}
-function inputoptions(){
-  if($('#options')!==undefined){
-    var lv= $('#options').html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#contentoptions').html('<input class="input" name="options" id="inputoptions" type="text" value="' + options + '"/>'); // remplace le code HTML actuel par celui-ci
-  }
-}
-function inputspe(){
-  if($('#spe')!==undefined){
-    var lv= $('#spe').html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#contentspe').html('<input class="input" name="spe" id="inputspe" type="text" value="' + spe + '"/>'); // remplace le code HTML actuel par celui-ci
+function input(type){
+  if($('#'+ type)!==undefined){
+    var contenu= $('#'+ type).html(); // renvoie le texte contenu à l'intérieur du paragraphe
+    $('#content' + type).html('<input class="input" name="'+type+'" id="input'+type+'" type="text" value="'+ contenu + '"/>'); // remplace le code HTML actuel par celui-ci
   }
 }
