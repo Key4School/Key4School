70b2e018af35f74e6f13190890da8dfe131ac843
Flavien <flavcodeur@gmail.com>
2021-04-18 11:21:12 +0200

    selectioner automatiquement l'input dans profil

--- static/js/profil.js
+++ static/js/profil.js
@@ -1,6 +1,6 @@
 function input(type){
   if($('#'+ type)!==undefined){
     var contenu= $('#'+ type).html(); // renvoie le texte contenu à l'intérieur du paragraphe
-    $('#content' + type).html('<input class="input" name="'+type+'" id="input'+type+'" type="text" value="'+ contenu + '"/>'); // remplace le code HTML actuel par celui-ci
+    $('#content' + type).html('<input onfocus="this.select();" onclick="this.select();" class="input" name="'+type+'" id="input'+type+'" type="text" value="'+ contenu + '"/>'); // remplace le code HTML actuel par celui-ci
   }
 }
