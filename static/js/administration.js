61a1932f04698c8a3006210e194327a6f431495b
tib500 <72920795+tib500@users.noreply.github.com>
2021-05-28 21:51:43 +0200

    admin

--- static/js/administration.js
+++ static/js/administration.js
@@ -1,29 +1,55 @@
-function supprimer(e,a) {
+function supprimer(e) {
   e.preventDefault();
-  var donnees = $('#demandeSuppr'+a).serialize();
-  $.ajax({
-    url: '/administration/', // on donne l'URL du fichier de traitement
-    type: "POST", // la requête est de type POST
-    data: donnees, // et on envoie nos données
-    success: function() {
-        testONLBN();
-    },
-  });
-}
-
-function valider(e) {
-  e.preventDefault();
-  var donnees = $('#demandeValider'+a).serialize();
+  var id = document.getElementById('idSupprimé').value;
+  var donnees = $('#supprForm').serialize();
   $.ajax({
     url: '/administration/', // on donne l'URL du fichier de traitement
     type: "POST", // la requête est de type POST
     data: donnees, // et on envoie nos données
     success: function(response) {
-       testONLBN();
+      document.getElementById('divDemande'+id).style.display='None';
+      supprimerClose();
     },
   });
 }
 
-testONLBN(){
-  document.getElementById('divTest').innerHTML='yass';
+function suppression(id){
+  document.getElementById("idSupprimé").value = id;
+  supprimerOpen();
+}
+
+function supprimerOpen(){
+  $("#suppression").addClass("is-active");
+}
+
+function supprimerClose(){
+  $("#suppression").removeClass("is-active");
+}
+
+function valider(e) {
+  e.preventDefault();
+  var id = document.getElementById('idValidé').value;
+  var donnees = $('#valForm').serialize();
+  $.ajax({
+    url: '/administration/', // on donne l'URL du fichier de traitement
+    type: "POST", // la requête est de type POST
+    data: donnees, // et on envoie nos données
+    success: function(response) {
+      document.getElementById('divDemande'+id).style.display='None';
+      validerClose();
+    },
+  });
+}
+
+function validation(id){
+  document.getElementById("idValidé").value = id;
+  validerOpen();
+}
+
+function validerOpen(){
+  $("#validation").addClass("is-active");
+}
+
+function validerClose(){
+  $("#validation").removeClass("is-active");
 }
