3a1c10643c2a758975899b817af61ce7399f1377
Bapt5 <drouillet.baptiste@gmail.com>
2021-06-13 12:37:00 +0200

    [notif] optimisation

--- static/js/notification.js
+++ static/js/notification.js
@@ -1,18 +1,6 @@
 const socket = io(`ws://${document.location.host}`);
-var nbNotif = document.querySelectorAll('#notifContent > .notif').length;
-if (nbNotif > 0) {
-  document.getElementById('nbNotif').style.display = "block";
-  document.getElementById('nbNotif').innerHTML = nbNotif;
-} else {
-  document.getElementById('nbNotif').style.display = "none";
-}
 
-socket.on('connect', function() {
-  socket.emit('connectToNotif');
-});
-
-socket.on('newNotif', (html) => {
-  document.getElementById('notifContent').innerHTML += html;
+function updateNotif(){
   var nbNotif = document.querySelectorAll('#notifContent > .notif').length;
   if (nbNotif > 0) {
     document.getElementById('nbNotif').style.display = "block";
@@ -20,4 +8,15 @@ socket.on('newNotif', (html) => {
   } else {
     document.getElementById('nbNotif').style.display = "none";
   }
+}
+
+updateNotif();
+
+socket.on('connect', function() {
+  socket.emit('connectToNotif');
+});
+
+socket.on('newNotif', (html) => {
+  document.getElementById('notifContent').innerHTML += html;
+  updateNotif()
 });
