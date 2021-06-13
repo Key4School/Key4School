60438fdc1496ea129d55649e7427d0329e1c784e
Flavien <flavcodeur@gmail.com>
2021-06-13 18:36:45 +0200

    publication widget

--- static/js/bandeau.js
+++ static/js/bandeau.js
@@ -1,16 +1,3 @@
-function remove(){
-  if (document.getElementById('bandeauDroite').style.display == "none"){
-    document.getElementById('bandeauDroite').style.display = "block";
-    document.getElementById('filters').style.width = "80%";
-    document.getElementById('contentPost').style.width = "80%";
-
-  }
-  else{
-    document.getElementById('bandeauDroite').style.display = "none";
-    document.getElementById('filters').style.width = "100%";
-    document.getElementById('contentPost').style.width = "100%";
-  }
-}
 function recherche(){
   if (document.getElementById('rechercheTel').style.display == "none"){
     document.getElementById('rechercheTel').style.display = "block";
