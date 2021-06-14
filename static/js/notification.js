2ffa019afff4ea86eb39ce301017471a303a1410
Bapt5 <drouillet.baptiste@gmail.com>
2021-06-14 11:45:04 +0200

    [notif] suppr all

--- static/js/notification.js
+++ static/js/notification.js
@@ -1,21 +1,30 @@
 const socket = io(`ws://${document.location.host}`);
 
-function updateNotif(){
+function updateNotif() {
   var nbNotif = document.querySelectorAll('#notifContent > .notif').length;
   if (nbNotif > 0) {
     document.getElementById('nbNotif').style.display = "block";
     document.getElementById('nbNotif').innerHTML = nbNotif;
     document.getElementById('noNotif').style.display = "none";
+    document.getElementById('toutLu').style.display = "block";
   } else {
     document.getElementById('nbNotif').style.display = "none";
     document.getElementById('noNotif').style.display = "block";
+    document.getElementById('toutLu').style.display = "none";
   }
 }
 
-function supprNotif(id){
+function supprNotif(id) {
   socket.emit('supprNotif', id);
   document.getElementById(id).remove();
   updateNotif();
+}
+
+function allSuppr() {
+  var notifications = document.querySelectorAll('#notifContent > .notif');
+  for (const notification of notifications) {
+    supprNotif(notification.id);
+  }
 }
 
 updateNotif();
