4fca228030ba8617de9f2bb588c56fc7f7e3f49d
Bapt5 <drouillet.baptiste@gmail.com>
2021-06-20 17:22:09 +0200

    jquery notif

--- static/js/notification.js
+++ static/js/notification.js
@@ -1,27 +1,27 @@
 const socket = io(`${document.location.protocol === 'https:' ? 'wss' : 'ws'}://${document.location.host}`);
 
 function updateNotif() {
-  var nbNotif = document.querySelectorAll('#notifContent > .notif').length;
+  var nbNotif = $('#notifContent > .notif').length;
   if (nbNotif > 0) {
-    document.getElementById('nbNotif').style.display = "block";
-    document.getElementById('nbNotif').innerHTML = nbNotif;
-    document.getElementById('noNotif').style.display = "none";
-    document.getElementById('toutLu').style.display = "block";
+    $('#nbNotif').css({'display': "block"});
+    $('#nbNotif').html(nbNotif);
+    $('#noNotif').css({'display': "none"});
+    $('#toutLu').css({'display': "block"});
   } else {
-    document.getElementById('nbNotif').style.display = "none";
-    document.getElementById('noNotif').style.display = "block";
-    document.getElementById('toutLu').style.display = "none";
+    $('#nbNotif').css({'display': "none"});
+    $('#noNotif').css({'display': "block"});
+    $('#toutLu').css({'display': "none"});
   }
 }
 
 function supprNotif(id) {
   socket.emit('supprNotif', id);
-  document.getElementById(id).remove();
+  $('#'+id).remove();
   updateNotif();
 }
 
 function allSuppr() {
-  var notifications = document.querySelectorAll('#notifContent > .notif');
+  var notifications = $('#notifContent > .notif');
   for (const notification of notifications) {
     supprNotif(notification.id);
   }
@@ -34,6 +34,6 @@ socket.on('connect', function() {
 });
 
 socket.on('newNotif', (html) => {
-  document.getElementById('notifContent').innerHTML += html;
+  $('#notifContent').append(html);
   updateNotif();
 });
