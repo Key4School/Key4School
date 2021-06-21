04986581f1792901451399190bd45cc82b3bb15b
Bapt5 <drouillet.baptiste@gmail.com>
2021-06-21 14:22:49 +0200

    fixe scroll infini

--- routing/accueil.py
+++ routing/accueil.py
@@ -13,7 +13,8 @@ def accueil():
         user = utilisateurs[session['id']].toDict()
         # subjects = getUserSubjects(user)
         # ici on récupère les 10 dernières demandes les plus récentes non résolues corresppondant aux matières de l'utilisateur
-        demandes = sorted([d.toDict() for d in demandes_aide.values() if d.matiere in user['matieres'] and not d.resolu and not d.id_utilisateur == ObjectId(session['id'])], key = lambda d: ( len(d['réponses associées']) + len(d['likes']) ), reverse=True)[:10]
+        demandes = sorted([d.toDict() for d in demandes_aide.values() if d.matiere in user['matieres'] and not d.resolu and not d.id_utilisateur == ObjectId(session['id'])],
+                    key = lambda d: ( len(d['réponses associées']) + len(d['likes']) ), reverse=True)[:10]
 
         return render_template("index.html", demandes=demandes, user=user)
     else:
