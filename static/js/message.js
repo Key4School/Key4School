2d620f0dbf53dd30c5e2245ebb23a02ee7bb1344
tib500 <72920795+tib500@users.noreply.github.com>
2021-06-25 13:45:14 +0200

    msg vocaux style lecteur (en dev)

--- static/js/message.js
+++ static/js/message.js
@@ -423,6 +423,111 @@ function changeRate(id) {
   }
 }
 
+
+
+
+  // const playIconContainer = document.getElementById('play-icon');
+  // const audioPlayerContainer = document.getElementById('audio-player-container');
+  // const seekSlider = document.getElementById('seek-slider');
+  // const audio = document.querySelector('audio');
+  // let playState = 'pause';
+  //
+  // playIconContainer.removeAttribute('onclick');
+  //
+  //
+  // playIconContainer.addEventListener('click', () => {
+  //     if(playState === 'play') {
+  //         audio.pause();
+  //         cancelAnimationFrame(raf);
+  //         playState = 'pause';
+  //         playIconContainer.innerHTML = "play";
+  //     } else {
+  //         audio.play();
+  //         requestAnimationFrame(whilePlaying);
+  //         playState = 'play';
+  //         playIconContainer.innerHTML = "pause";
+  //     }
+  // });
+  //
+  //
+  //
+  // const showRangeProgress = (rangeInput) => {
+  //     if(rangeInput === seekSlider) audioPlayerContainer.style.setProperty('--seek-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+  // }
+  //
+  // seekSlider.addEventListener('input', (e) => {
+  //     showRangeProgress(e.target);
+  // });
+  //
+  // const durationContainer = document.getElementById('duration');
+  // const currentTimeContainer = document.getElementById('current-time');
+  // let raf = null;
+  //
+  // const calculateTime = (secs) => {
+  //     const minutes = Math.floor(secs / 60);
+  //     const seconds = Math.floor(secs % 60);
+  //     const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  //     return `${minutes}:${returnedSeconds}`;
+  // }
+  //
+  // const displayDuration = () => {
+  //     durationContainer.textContent = calculateTime(audio.duration);
+  // }
+  //
+  // const setSliderMax = () => {
+  //     seekSlider.max = Math.floor(audio.duration);
+  // }
+  //
+  // const displayBufferedAmount = () => {
+  //     const bufferedAmount = Math.floor(audio.buffered.end(audio.buffered.length - 1));
+  //     audioPlayerContainer.style.setProperty('--buffered-width', `${(bufferedAmount / seekSlider.max) * 100}%`);
+  // }
+  //
+  // const whilePlaying = () => {
+  //     seekSlider.value = Math.floor(audio.currentTime);
+  //     currentTimeContainer.textContent = calculateTime(seekSlider.value);
+  //     audioPlayerContainer.style.setProperty('--seek-before-width', `${seekSlider.value / seekSlider.max * 100}%`);
+  //     raf = requestAnimationFrame(whilePlaying);
+  // }
+  //
+  // if (audio.readyState > 0) {
+  //     displayDuration();
+  //     setSliderMax();
+  //     displayBufferedAmount();
+  // } else {
+  //     audio.addEventListener('loadedmetadata', () => {
+  //         displayDuration();
+  //         setSliderMax();
+  //         displayBufferedAmount();
+  //     });
+  // }
+  //
+  // audio.addEventListener('progress', displayBufferedAmount);
+  //
+  // seekSlider.addEventListener('input', () => {
+  //     currentTimeContainer.textContent = calculateTime(seekSlider.value);
+  //     if(!audio.paused) {
+  //         cancelAnimationFrame(raf);
+  //     }
+  // });
+  //
+  // seekSlider.addEventListener('change', () => {
+  //     audio.currentTime = seekSlider.value;
+  //     if(!audio.paused) {
+  //         requestAnimationFrame(whilePlaying);
+  //     }
+  // });
+  //
+  // audio.addEventListener("ended", function(){
+  //      audio.currentTime = 0;
+  //      playState = 'pause';
+  //      playIconContainer.innerHTML = "play";
+  // });
+// }
+
+
+
+
 function signalisationDiscussion() {
   selectionSign = document.getElementById("sign").className
   if (selectionSign == "far fa-flag") {
