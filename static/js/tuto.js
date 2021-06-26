c794252db53d8a042a74d39ea0b5ad678b38acdc
ludovicjlv <81871401+ludovicjlv@users.noreply.github.com>
2021-06-26 17:55:07 +0200

    update tuto

--- static/js/tuto.js
+++ static/js/tuto.js
@@ -1,36 +1,51 @@
 var page = 0;
-var imageMax = 5;
+var imageMax = 8;
 
 var data = [
   {
-    'texte': "Après votre première connexion sur le site vous trouverez, sur la page d'accueil, les dernières questions postées sur le site.",
-    'titre': "1. Accueil",
+    'texte': "Bienvenue sur key4school ! Nous sommes ravis de votre arrivée ! key4school est un réseau social développé par 12 lycéens d’Ile de France a destination des lycéens. L’objectif est d’y trouver de l’aide et de s’entraider facilement.",
+    'titre': "1. Présentation", 
     'lien': "/"
   },
   {
-    'texte': "Les matière définissent les questions que vous souhaitez voir. Si l'on ne veut voir que les questions de français, on décoche donc toutes les matières qui ne nous intéressent pas.",
-    'titre': "2. Filtrer les demandes d'aide",
+    'texte': "Dès votre arrivée sur le site, vous êtes sur la page d’accueil où se trouvent les toutes dernières questions posées par d’autres membres. Les questions ne concernent uniquement les matières que vous étudiez, pour en sélectionner une en particulier vous pouvez utiliser les filtres en haut de page. Si en défilant vous tombez sur une question à laquelle vous pouvez répondre alors n’hésitez-pas, il vous suffit de cliquer sur la bulle en dessous ! ",
+    'titre': "2. Accueil",
     'lien': "/"
   },
   {
-    'texte': 'Pour poser une question, rien de plus simple ! Cliquez sur "Poser une question" et une interface vous sera proposée. Il suffit de remplir l\'objet de votre question ainsi que le détail afin que la communauté vous aide au mieux. Si vous avez une photo du problème n\'hésitez pas à l\'ajouter en cliquant sur "Fichier".',
+    'texte': 'Quand vous avez une question à poser, vous pouvez dans un premier temps taper les mots-clés dans la barre de recherche et regarder s’il y en a déjà une qui y répond. Si vous ne trouvez pas votre réponse, vous n’avez qu’à cliquer sur « Poser une question » et remplir le formulaire qui vous ai donné. Vous pouvez également joindre un fichier comme un exercice par exemple. Essayez d’être poli et le plus clair possible. Une fois que vous estimez que vous avez obtenu réponse à votre question, vous n’avez plus qu’à la mettre en tant que résolue afin de fermer le sujet.',
     'titre': "3. Poser une question",
     'lien': "/question/"
   },
   {
-    'texte': "Dans le menu déroulant à droite de votre écran, vous trouverez l'onglet \"Messages privés\". En appuyant sur le \"+\" vous pouvez chercher d'autres utilisateurs afin de débuter une conversation avec eux. Vous pouvez également envoyer des messages vocaux.",
-    'titre': "4. Les messages privés",
+    'texte': "En dessous de chaque question, vous avez la possibilité de l’enregistrer afin de pouvoir la retrouver dans la page correspondante accessible dans le menu en haut à droite. Cela vous permet ainsi soit d’y répondre plus tard soit d’être informé si elle est résolue par quelqu’un. ",
+    'titre': "4. Demandes enregistrées",
+    'lien': "/saved/"
+  },
+  {
+    'texte': "Ils sont accessibles à partir du menu en haut à droite, vous pouvez créer des groupes avec le « + », envoyer des messages, des vocaux et même des images ! Vous retrouverez également de base un groupe avec les gens de votre classe afin d’échanger simplement. ",
+    'titre': "5. Messages privés",
     'lien': "/messages/"
   },
   {
-    'texte': "Sur votre profil vous pouvez renseigner vos informations comme votre pseudo, vos langues vivantes, votre adresse mail, votre numéro de téléphone, bien évidemment vous pouvez choisir de les rendre publiques ou non.",
-    'titre': "5. Votre Profil",
+    'texte': "Accessibles en haut à droite de votre écran, elles vous indiquent si vous avez un nouveau message ou si quelqu’un a répondu à une de vos questions / une question que vous avez enregistrée.",
+    'titre': "6. Notifications",
+    'lien': "/accueil/"
+  },
+  {
+    'texte': "Accessible depuis le menu déroulant, il vous permet de renseigner vos informations comme votre pseudo, vos langues vivantes, options et spécialités. Vous pouvez également ajouter une photo de profil ou ajouter des moyens de contact en décidant bien évidemment de les rendre publiques ou non. Vous y retrouverez aussi vos questions posées. Nous vous invitons à le compléter dès votre première visite !",
+    'titre': "7. Profil",
     'lien': "/profil/"
   },
   {
-    'texte': "Le système d'XP permet de récompenser votre contribution sur le site. Il vous permet de débloquer des récompenses uniques en fonction de votre niveau, comme par exemple un large choix de thèmes pour le site lui-même. Vos récompenses ainsi que votre niveau apparaissent sur votre page de profil ",
-    'titre': "6. Le système d'XP",
+    'texte': "Il permet de récompenser votre contribution sur le site. Vous pouvez ainsi débloquer des récompenses uniques en fonction de votre niveau, comme un large choix de thèmes pour le site lui-même. Vos récompenses ainsi que votre niveau apparaissent sur votre page de profil.",
+    'titre': "8. Système d'XP",
     'lien': "/XP_tuto/"
+  },
+  {
+    'texte': "Afin de profiter d’une expérience agréable sur key4school, nous vous demandons de toujours rester courtois et bienveillant. Sachez qu’il existe un système d’auto-modération qui concerne les questions et leurs réponses, il est également possible de l’activer dans n’importe quel groupe de messages dans les paramètres de celui-ci. Dans tous les cas, si vous remarquez quelque chose qui est déplacé et ne devrait pas se trouvez sur key4school, vous pouvez le signaler et des sanctions peuvent être prises.",
+    'titre': "9. Veiller au bon déroulement de votre passage sur key4school",
+    'lien': "/accueil/"
   }
 ]
 
@@ -43,8 +58,8 @@ function update(){
 
 function suivant() {
   page += 1;
-  if (page > 5) {
-    page = 5;
+  if (page > 8) {
+    page = 8;
   }
   if (page == imageMax) {
     document.getElementById("next").style.display = "none";
