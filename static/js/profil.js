2851b36848fea8809e0968c6703533843940e081
Bapt5 <drouillet.baptiste@gmail.com>
2021-07-06 16:09:19 +0200

    fixe space

--- static/js/profil.js
+++ static/js/profil.js
@@ -1,6 +1,6 @@
 function input(type){
   if($('#'+ type)!==undefined){
-    var contenu= $('#'+ type).html(); // renvoie le texte contenu à l'intérieur du paragraphe
+    var contenu= $('#'+ type).html().trim(); // renvoie le texte contenu à l'intérieur du paragraphe
     if (!contenu.includes('Non-renseigné')){
       $('#content' + type).html('<input onfocus="this.select();" onclick="this.select();" class="input" name="'+type+'" id="input'+type+'" type="text" value="'+ contenu + '"/>'); // remplace le code HTML actuel par celui-ci
     }else{
