2cfe99162457d4d9db09e23ca9955ea0111b3264
JulienPierr <mail.julien.pierru@gmail.com>
2021-07-22 12:05:20 +0200

    Migration demandes d'aide

--- migrationDB.py
+++ migrationDB.py
@@ -13,6 +13,7 @@ from email.mime.text import MIMEText
 from threading import Thread
 from matieresDict import translations, translateProf
 from uuid import uuid4
+import json
 
 def create_connection(db_name, db_user, db_password):
     connection = None
@@ -28,7 +29,7 @@ def create_connection(db_name, db_user, db_password):
     return connection
 
 
-connection = create_connection("nomdeladb", "nomdel'utilisateur","motdepassedeladb")
+connection = create_connection("key4school", "postgres","")
 app = Flask(__name__)
 
 try :
@@ -42,6 +43,10 @@ except :
 
 with app.test_request_context(
         '/make_report/2017', data={'format': 'short'}):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    psycopg2.extras.register_uuid()
+    
     for a in utilisateurs:
         e = utilisateurs[a].toDict()
         _id = uuid4()
@@ -78,18 +83,68 @@ with app.test_request_context(
         admin = e.get('admin', False)
         savedDemands = e.get('savedDemands', [ObjectId('60d8b3a48a701d9f2cbebf3c')])
 
-        connection.autocommit = True
-        cursor = connection.cursor()
-        psycopg2.extras.register_uuid()
-
         try:
             print(_id,nom,prenom,pseudo,nomImg,dateInscription,birth_date,classe,lycee,spes,langues,options,matiere,matiere_autre,couleur,typeEleve,elementPublic,elementPrive,caractere,email,interets,telephone,notifs,Sanctions,SanctionEnCour,SanctionDuree,xp,xpModeration,admin)
             cursor.execute("INSERT INTO users (id, name, firstname, pseudo, image_name, inscription_date, birth_date, grade, school, spes, languages, options, matter, other_matter, color, type, public, private, mood, email, interest, phone, sanction, current_sanction, sanction_duration, xp, xp_moderation, admin) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s)", (_id,nom,prenom,pseudo,nomImg,dateInscription,birth_date,classe,lycee,spes,langues,options,matiere,matiere_autre,couleur,typeEleve,elementPublic,elementPrive,caractere,email,interets,telephone,Sanctions,SanctionEnCour,SanctionDuree,xp,xpModeration,admin))
             print("Query executed successfully")
         except OperationalError as e:
             print(f"The error '{e}' occurred")
+        
+
+    for a in demandes_aide:
+        e = demandes_aide[a].toDict()
+        _id = uuid4()
+        id_utilisateur = uuid4()
+        titre = e['titre']
+        contenu = e['contenu']
+        date_envoi = e['date-envoi']
+        matiere = e['matière']
+        likes = e['likes']
+        motif = e.get('motif', [])
+        for x in range(len(motif)):
+            _id_sign = uuid4()
+            try:
+                cursor.execute("INSERT INTO sign (id, id_ask, reason) VALUES (%s, %s, %s)", (_id_sign, _id, motif[x].get('txt')))
+                print("Query executed successfully")
+            except OperationalError as e:
+                print(f"The error '{e}' occurred")
+        resolu = e['resolu']
+        fileType = e['fileType']
+        try:
+            cursor.execute("INSERT INTO ask_for_help (id, id_user, title, content, sending_date, matter, likes, resolved, filetype) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s)", (_id, id_utilisateur, titre, contenu, date_envoi, matiere, likes, resolu, fileType))
+            print("Query executed successfully")
+        except OperationalError as e:
+            print(f"The error '{e}' occurred")
+        answers = e['réponses associées']
+        for i in range(len(answers)) :
+            _id_answer = uuid4()
+            id_utilisateur_answer = uuid4()
+            content = answers[i].get('contenu')
+            date = answers[i].get('date-envoi')
+            like = answers[i].get('likes')
+            motif_answers = answers[i].get('motif')
+
+            
+            try:
+                cursor.execute("INSERT INTO answers (id, id_user, id_ask, content, sending_date, likes) VALUES (%s, %s, %s,%s, %s, %s)", (_id_answer, id_utilisateur_answer, _id, content, date, like))
+                print("Query executed successfully")
+            except OperationalError as e:
+                print(f"The error '{e}' occurred")
+            
+            for b in range(len(motif_answers)):
+                id_sign = uuid4()
+                try:
+                    cursor.execute("INSERT INTO sign (id, id_ask, reason) VALUES (%s, %s, %s)", (id_sign, _id_answer, motif_answers[b].get('txt')))
+                    print("Query executed successfully")
+                except OperationalError as e:
+                    print(f"The error '{e}' occurred")
+            
+
+            
 
 
-if connection:
+
+
+if connection :
     connection.close()
     print("PostgreSQL connection is closed")
