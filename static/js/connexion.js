acb8eb8c805e6cdea8c12c5df2c39376b34d8c7a
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2021-10-02 12:00:11 +0200

    img connexion

---
+++ static/js/connexion.js
@@ -0,0 +1,12 @@
+var num=0;//num ninimum
+var numMax=1;//nombre d'image a faire defilé +1
+
+
+function photo(){
+  $("#img_connexion").attr('src','../static/image/publication/pub'+num+'.png');
+  num+=1;
+  if (num==numMax){
+    num=0
+  }
+}
+setInterval(photo, 5000);
