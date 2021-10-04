8394375be3a241d30c26faad04cd8bd81c847d38
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2021-10-04 12:09:11 +0200

    img sur connexion

--- static/js/connexion.js
+++ static/js/connexion.js
@@ -1,12 +1,13 @@
 var num=0;//num ninimum
-var numMax=1;//nombre d'image a faire defilé +1
-
+var numMax=2;//nombre d'image a faire defilé +1
+var img=['Entraide.gif', 'pub0.png'];
 
 function photo(){
-  $("#img_connexion").attr('src','../static/image/connexion/pub'+num+'.png');
-  num+=1;
+  $("#img_connexion").attr('src','../static/image/connexion/'+img[num]);
+    num+=1;
   if (num==numMax){
     num=0
   }
+
 }
-setInterval(photo, 5000);
+setInterval(photo, 10000);
