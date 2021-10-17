f013eae3172e2c1f04ec9fd3d1735b35c4f4caa0
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2021-10-17 19:45:41 +0200

    msg Vocaux/taille icone/fix bug couleur profil/new publication

--- static/js/connexion.js
+++ static/js/connexion.js
@@ -1,6 +1,6 @@
 var num=0;//num ninimum
-var numMax=2;//nombre d'image a faire defilé +1
-var img=['Entraide.gif', 'everywhere.gif'];
+var numMax=3;//nombre d'image a faire defilé +1
+var img=['Entraide.gif', 'everywhere.gif', "Une messagerie instantanée.gif"];
 
 function photo(){
   $("#img_connexion").attr('src','../static/image/connexion/'+img[num]);
