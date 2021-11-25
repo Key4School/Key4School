ebdeba83eb444190260c0e01d7b45e881b9f7ed8
Flavien Mathieu <72614724+Mflavien01@users.noreply.github.com>
2021-11-25 08:18:18 +0100

    post leaderboard on connexion

--- static/js/connexion.js
+++ static/js/connexion.js
@@ -1,12 +1,12 @@
-var num=0;//num ninimum
-var numMax=3;//nombre d'image a faire defilé +1
-var img=['Entraide.gif', 'everywhere.gif', "Une messagerie instantanée.gif"];
+var num = 0;
+var numMax = 4;
+var img = ['Entraide.gif', 'everywhere.gif', "Une messagerie instantanée.gif", 'LeaderBoard.gif'];
 
-function photo(){
-  $("#img_connexion").attr('src','../static/image/connexion/'+img[num]);
-    num+=1;
-  if (num==numMax){
-    num=0
+function photo() {
+  $("#img_connexion").attr('src', '../static/image/connexion/' + img[num]);
+  num += 1;
+  if (num == img.length) {
+    num = 0
   }
 }
 setInterval(photo, 10000);
