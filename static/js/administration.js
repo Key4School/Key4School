959f3f17b525100d5302ac87f3e2d3fbee6f74fe
Bapt5 <drouillet.baptiste@gmail.com>
2021-12-08 22:41:10 +0100

    [Bug] move fct sign to signalement.js

--- static/js/administration.js
+++ static/js/administration.js
@@ -210,68 +210,10 @@ function valDisc(e) {
   });
 }
 
-
-
 function valDiscOpen() {
   $("#valDisc").addClass("is-active");
 }
 
 function valDiscClose() {
   $("#valDisc").removeClass("is-active");
-}
-function signalisationProfil() {
-  selectionSign = document.getElementById("signProfil").className
-  if (selectionSign == "far fa-flag") {
-    signalisationProfilOpen();
-
-
-  }
-  if (selectionSign == "fas fa-flag") {
-    designalisationProfilOpen();
-  }
-}
-
-function signalisationProfilOpen() {
-  $("#signalisationProfil").addClass("is-active");
-}
-
-function signalisationProfilClose() {
-  $("#signalisationProfil").removeClass("is-active");
-}
-
-function designalisationProfilOpen() {
-  $("#designalisationProfil").addClass("is-active");
-}
-
-function designalisationProfilClose() {
-  $("#designalisationProfil").removeClass("is-active");
-}
-
-function signalerProfil(e) {
-  e.preventDefault();
-  var donnees = $('#signalementProfil').serialize();
-  $.ajax({
-    url: '/signPostProfil/', // on donne l'URL du fichier de traitement
-    type: "POST", // la requête est de type POST
-    data: donnees, // et on envoie nos données
-    success: function(response) {
-      $('#signalementProfil').trigger("reset");
-      signalisationProfilClose();
-      document.getElementById("signProfil").className = "fas fa-flag";
-    },
-  });
-}
-
-function designalerProfil(e) {
-  e.preventDefault();
-  var donnees = $('#signalementProfil').serialize();
-  $.ajax({
-    url: '/signPostProfil/', // on donne l'URL du fichier de traitement
-    type: "POST", // la requête est de type POST
-    data: donnees, // et on envoie nos données
-    success: function(response) {
-      designalisationProfilClose();
-      document.getElementById("signProfil").className = "far fa-flag";
-    },
-  });
 }
