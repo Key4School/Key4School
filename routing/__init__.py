01c414d7d26c44df0a7ca4ac56c579f434f6a537
Bapt5 <drouillet.baptiste@gmail.com>
2022-01-05 22:36:14 +0100

    [Routing] beautify

--- routing/__init__.py
+++ routing/__init__.py
@@ -1,10 +1,22 @@
-from routing.accueil import accueil, tuto, XP_tuto, mail_rendu, saved, about, leaderboard
-from routing.administration import administration, suppressionMsg, validerMsg, sanction, signPost, signRepPost, signPostProfil, signPostDiscussion, signPostMsg
-from routing.demandes_aide import question, comments, updateDemand, updateComment, file, DL_file, likePost, likeRep, resoudre, savePost
+from routing.accueil import (XP_tuto, about, accueil, leaderboard, mail_rendu,
+                             saved, tuto)
+from routing.administration import (administration, sanction, signPost,
+                                    signPostDiscussion, signPostMsg,
+                                    signPostProfil, signRepPost,
+                                    suppressionMsg, validerMsg)
+from routing.demandes_aide import (DL_file, comments, file, likePost, likeRep,
+                                   question, resoudre, savePost, updateComment,
+                                   updateDemand)
 # from routing.flask_error import custom_404
 from routing.functions import afficheNotif
-from routing.login import login, signIn0, signIn1, signIn2, logout
-from routing.messages import page_messages, redirectDM, uploadAudio, audio, uploadImage, image, createGroupe, updateGroupe, virerParticipant, modifRole, supprGroupe, updateGrpName, moreMsg, modererGrp
-from routing.profil import profil, changeTheme, theme, updateprofile, userImg, updateImg, otherSubject, topLeaderboard, deleteAccount, emailNotVerify, emailVerification
-from routing.recherche import recherche, recherche_user, morePost, moreUser
-from routing.sockets import connectToNotif, disconnect, supprNotif, connectToGroup, postMsg, postLike
+from routing.login import login, logout, signIn0, signIn1, signIn2
+from routing.messages import (audio, createGroupe, image, modererGrp,
+                              modifRole, moreMsg, page_messages, redirectDM,
+                              supprGroupe, updateGroupe, updateGrpName,
+                              uploadAudio, uploadImage, virerParticipant)
+from routing.profil import (changeTheme, deleteAccount, emailNotVerify,
+                            emailVerification, otherSubject, profil, theme,
+                            topLeaderboard, updateImg, updateprofile, userImg)
+from routing.recherche import morePost, moreUser, recherche, recherche_user
+from routing.sockets import (connectToGroup, connectToNotif, disconnect,
+                             postLike, postMsg, supprNotif)
